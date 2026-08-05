3e60323fcfd369c69ae2bd0c0707f0a4d438e36f
handoyom04-rgb <handoyom04@gmail.com>
2026-08-05 08:16:00 +0700

    Add files via upload

---
+++ program_pertama.py
@@ -0,0 +1,38 @@
+# program pertamaku
+# namaku: [hasan]
+# kelas: X RPL
+
+print("========================================")
+print("      SELAMAT DATANG DI PYTHON !")
+print("========================================")
+print()
+print("program ini dibuat oleh", "[M hasan H]")
+print("sekolah: smk TJP tuban")
+print("tahun    :", 2026)
+print()
+print(10+5)
+print(20-8)
+print(4*7)
+print(15/4)
+print(15//4)
+print(15%4)
+print(2**8)
+print()
+a=24
+b=7
+c=8
+print("penjumblahan::", a+b )
+print()
+
+
+
+
+
+
+
+
+
+
+
+
+

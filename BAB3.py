37aed717fc2fbe5774446599a4093deed5074826
handoyom04-rgb <handoyom04@gmail.com>
2026-08-05 12:41:26 +0700

    Add files via upload

---
+++ BAB3.py
@@ -0,0 +1,25 @@
+Nama = "M.hasan.H"
+umur = 16
+Tinggi = 159.8
+Lulus = "false"
+print()
+print()
+print()
+print ("nama saya adalah:", Nama)
+print ("umur saya:", umur)
+print ("tinggi saya:", Tinggi)
+print ("tidak lulus:", Lulus)
+print()
+print()
+print()
+Nama_Produk = "sepatu lari"
+Stok_Tersedia = 15
+Harga = 249.5
+Tersedia_di_Toko = True
+print()
+print()
+print()
+print ("Nama Produk:", Nama_Produk)
+print ("Stok Tersedia:", Stok_Tersedia )
+print ("Harga Produk ($):", Harga)
+print ("Tersedia di Toko:", Tersedia_di_Toko)
